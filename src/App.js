e0596b5a0d7d70afcee1833c8eceb71e62d77cf3
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-24 10:07:14 +0530

    added store to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { Provider } from "react-redux";
 import { Contact } from "./components/contacts/Contact";
 import { NavBar } from "./components/NavBar";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container">
-        <div className="py-3">
-          <Contact />
+    <Provider store={store}>
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          <div className="py-3">
+            <Contact />
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
