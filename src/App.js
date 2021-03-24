8ad57e55de8793408905f2376917138d39dfe460
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-24 07:17:00 +0530

    added conatct component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Contact } from "./components/contacts/Contact";
 import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
+        <div className="py-3">
+          <Contact />
+        </div>
+      </div>
     </div>
   );
 }
