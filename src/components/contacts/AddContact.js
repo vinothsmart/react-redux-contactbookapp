e9cd3459e4964b9a98c8540b8727df99d6819f08
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-25 11:56:00 +0530

    add Contact with submit values

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,3 +1,62 @@
+import { useState } from "react";
+
 export const AddContact = () => {
-  return <div>add contact</div>;
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const createContact = (e) => {
+    e.preventDefault();
+    console.log("name", name);
+    console.log("phone", phone);
+    console.log("email", email);
+  };
+
+  return (
+    <div>
+      <div className="card border-0 shadow">
+        <div className="card-header">Add a contact</div>
+        <div className="card-body">
+          <form onSubmit={(e) => createContact(e)}>
+            <div className="form-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="enter your name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="enter your phone number"
+                value={phone}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="enter your email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
