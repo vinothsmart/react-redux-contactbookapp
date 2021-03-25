c4f99e35e5c3924eb73b12f69b7d9c2a55c7f384
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-25 15:44:16 +0530

    Root reducer added to store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
-const store = createStore(contactReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
