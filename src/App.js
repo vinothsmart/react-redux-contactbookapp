a8ac5ffe6922b0ecae4d7405b15cf06f647bdecd
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-25 10:05:06 +0530

    added react router in application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Provider } from "react-redux";
 import { Contact } from "./components/contacts/Contact";
 import { NavBar } from "./components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <NavBar />
-        <div className="container">
-          <div className="py-3">
-            <Contact />
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <div className="py-3">
+              <Contact />
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     </Provider>
   );
 }
