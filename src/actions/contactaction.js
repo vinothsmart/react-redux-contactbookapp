226e6a87d6d4e5a8a3280b857c09e2ee35d6112a
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-26 15:04:11 +0530

    add action updated

--- src/actions/contactaction.js
+++ src/actions/contactaction.js
@@ -1,8 +1,6 @@
 import { CREATE_CONTACT } from "../constant/types";
 
-export const addContact = (contact) => {
-  return {
-    type: CREATE_CONTACT,
-    payload: contact,
-  };
-};
+export const addContact = (contact) => ({
+  type: CREATE_CONTACT,
+  payload: contact,
+});
