5982f2607f8830186af6103f727145d06973e84a
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-29 10:44:10 +0530

    delete action created

--- src/actions/contactaction.js
+++ src/actions/contactaction.js
@@ -1,16 +1,30 @@
-import { CREATE_CONTACT, GET_CONTACT, UPDATE_CONTACT } from "../constant/types";
+import {
+  CREATE_CONTACT,
+  GET_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+} from "../constant/types";
 
+// add contact
 export const addContact = (contact) => ({
   type: CREATE_CONTACT,
   payload: contact,
 });
 
+// get contact
 export const getContact = (id) => ({
   type: GET_CONTACT,
   payload: id,
 });
 
+// update contact
 export const updateContact = (contact) => ({
   type: UPDATE_CONTACT,
   payload: contact,
+});
+
+// delete contact
+export const deleteContact = (id) => ({
+  type: DELETE_CONTACT,
+  payload: id,
 });
