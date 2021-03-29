c2d64d578d5727c3de06f44871e88134b0d3266b
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-29 10:20:50 +0530

    Update contact reducer created

--- src/reducers/contactreducer.js
+++ src/reducers/contactreducer.js
@@ -1,4 +1,4 @@
-import { CREATE_CONTACT, GET_CONTACT } from "../constant/types";
+import { CREATE_CONTACT, GET_CONTACT, UPDATE_CONTACT } from "../constant/types";
 
 const initalState = {
   contacts: [
@@ -254,6 +254,13 @@ export const contactReducer = (state = initalState, action) => {
       return {
         ...state,
         contact: arr,
+      };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact.id == action.payload.id ? action.payload : contact
+        ),
       };
     default:
       return state;
