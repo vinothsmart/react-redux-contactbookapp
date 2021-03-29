e71871074f404b34d0e9aff1e2614458cdf3646b
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-29 12:29:35 +0530

    select all using useState

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Contacts } from "./Contacts";
 
 export const Contact = () => {
+  const [SelectAll, setSelectAll] = useState(false);
   const contacts = useSelector((state) => state.contacts.contacts);
   console.log("contacts", contacts);
   return (
@@ -9,6 +11,23 @@ export const Contact = () => {
       <table className="table table-shadow table-striped">
         <thead>
           <tr>
+            <th scope="col">
+              <div className="custom-control custom-checkbox">
+                <input
+                  id="selectAll"
+                  type="checkbox"
+                  value={SelectAll}
+                  className="custom-control-input"
+                  onClick={() => {
+                    setSelectAll(!SelectAll);
+                  }}
+                />
+                <label
+                  htmlFor="selectAll"
+                  className="custom-control-label"
+                ></label>
+              </div>
+            </th>
             <th scope="col">id</th>
             <th scope="col">Name</th>
             <th scope="col">Phone</th>
